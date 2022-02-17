abd347527f7711fa29c6f6e959e0ae4fcb30b390
mahmou7789d <mahmoudha7789@gmail.com>
2022-02-17 15:57:39 -0800

    Code of AVR_1

---
+++ AVR_1/AVR_1/AVR_1/APP/AVR_1.c
@@ -0,0 +1,18 @@
+/*
+ * AVR_1.c
+ *
+ * Created: 2/17/2022 3:45:06 PM
+ *  Author: Mahm Oud Sa Ad
+ */ 
+
+#include <USART_File.h>
+#include <LCD.h>
+#include <LED.h>
+#include <Keypad.h>
+int main(void)
+{
+    while(1)
+    {
+        
+    }
+}
