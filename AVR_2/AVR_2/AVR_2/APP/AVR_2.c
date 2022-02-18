7a0018126f4876898ce863b0b7225af251bb83ea
mahmou7789d <mahmoudha7789@gmail.com>
2022-02-17 16:08:55 -0800

    Code of AVR 2

---
+++ AVR_2/AVR_2/AVR_2/APP/AVR_2.c
@@ -0,0 +1,19 @@
+/*
+ * AVR_2.c
+ *
+ * Created: 2/17/2022 4:00:52 PM
+ *  Author: Mahm Oud Sa Ad
+ */ 
+
+
+#include <USART_File.h>
+#include <LCD.h>
+#include <LED.h>
+
+int main(void)
+{
+    while(1)
+    {
+        
+    }
+}
